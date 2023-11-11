ef00c985a877bcb9deb6b22883c7ce78627d389b
Jae Hyune Yea <jaehyune.yea@gmail.com>
2023-11-10 17:57:58 -0800

    Create empty window

--- PhysicsEngine/PhysicsEngine.cpp
+++ PhysicsEngine/PhysicsEngine.cpp
@@ -5,9 +5,40 @@
 #include "PhysicsEngine.h"
 
 using namespace std;
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
 
 int main(int argc, char* argv[])
 {
+	// initialize SDL subsystems here
+	SDL_Init(SDL_INIT_VIDEO); 
+
+	SDL_Window* window = SDL_CreateWindow("Physics Engine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, 0);
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+
+	bool running = 1;
+	while (running) {
+		SDL_Event event;
+		SDL_PollEvent(&event);
+		switch (event.type)
+		{
+			case SDL_QUIT:
+			{
+				running = false;
+				break;
+			}
+		}
+	}
+
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+	SDL_RenderClear(renderer);
+
+	// destroy reference on exit
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+
 	cout << "Hello CMake." << endl;
+
+	SDL_Quit();
 	return 0;
 }
